4769f6256f2de1ce17e5d3d1df4552a5734791ff
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-07-09 00:19:31 -0500

    Finished the form to upload question, conection with firebase success too :happy:

---
+++ src/views/Add-question.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <h1 class="text-center">Añadir Pregunta</h1>
+        <vs-row>
+            <vs-col vs-w="6">
+                <label>Titulo</label>
+                <input type="text" v-model="title" placeholder="De que color es el caballo de napoleon?"/>
+                <br><br>
+                <label>Contenido</label>
+                <textarea v-model="content" placeholder="Habia un caballo bonito..."></textarea>
+            </vs-col>
+        </vs-row>
+        <vs-divider>
+            <h2>Respuestas</h2>
+        </vs-divider>
+        <vs-row>
+            <vs-col vs-w="6">
+                <div v-for="answer in answers" :key="answer.id">
+                    <input type="text" v-model="answer.name" 
+                        placeholder="El caballo se llama viernes"/>
+                        <br>
+                    <vs-checkbox @change="onChangeIsTrue(answer.id)" v-model="answer.isTrue">Es la respuesta correcta?</vs-checkbox>
+                    <br><br>
+                </div>            
+            </vs-col>
+        </vs-row>
+        <vs-row>
+            <vs-col vs-w="6" class="text-left">
+                <vs-button @click="uploadQuestion()" vs-color="secondary">Subir</vs-button>
+            </vs-col>
+        </vs-row>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+    name: 'AddQuestion',
+    data () {
+        return {
+            title: '',
+            content: '',
+            answers: [
+                {id: 0, name: '', isTrue: true},
+                {id: 1, name: '', isTrue: false},
+                {id: 2, name: '', isTrue: false}
+            ],
+            actualTrue: 0
+        }
+    },
+    methods: {
+        onChangeIsTrue (key) {
+            this.answers[this.actualTrue].isTrue = false
+            this.answers[key].isTrue = true
+            this.actualTrue = key
+        },
+
+        uploadQuestion () {
+            if (this.title && this.checkNullAnswers()) {
+                const dbRef = firebase.database().ref('/questions/').push()
+
+                dbRef.set({
+                    title: this.title,
+                    content: this.content,
+                    answers: this.answers
+                }).then(res => {
+                    this.$vs.notify({
+                        title: 'Exito!',
+                        text: 'La pregunta se ha agregado correctamente',
+                        color: 'success'
+                    })
+
+                    this.$router.push(`/question/${dbRef.key}`)
+                }).catch(error => {
+                    console.log(error)
+                    this.$vs.notify({
+                        title: 'Error message',
+                        text: error.message,
+                        color: 'danger'
+                    })
+                })
+            } else {
+                this.$vs.notify({
+                    title: 'Alerta',
+                    text: 'No debes de dejar las respuestas ni el titulo en blanco',
+                    color: 'warning'
+                })
+            }
+        },
+
+        checkNullAnswers () {
+            return this.answers[0].name && this.answers[1].name && this.answers[2].name
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .vs-row {
+        display: flex;
+        justify-content: center;
+    }
+    h1 {
+        margin-bottom: .5em;
+    }
+    .con-vs-checkbox {
+        display: flex;
+        justify-content: flex-start;
+    }
+</style>
+
