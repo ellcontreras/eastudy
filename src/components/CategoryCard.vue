60c1346471d11d4ef1a69ddcfc690434e783d897
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-07-08 19:36:33 -0500

    Category card component ready for the initial tests

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <vs-card vs-color="secondary">
+        <vs-card-header :vs-title="name" vs-background-color="danger" :vs-fill="true"/>
+        <vs-card-body>
+            {{ name }}
+        </vs-card-body>
+    </vs-card>
+</template>
+
+<script>
+export default {
+    name: 'CategoryCard',
+    props: {
+        name: String
+    }
+}
+</script>
+
