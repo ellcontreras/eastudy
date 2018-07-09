5293aa3904086d6b431b12fdbd8b5b876b95d2c2
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-07-09 00:20:30 -0500

    Added dependency firebase and integration with the app

--- src/App.vue
+++ src/App.vue
@@ -30,4 +30,31 @@ export default {
 #main-container {
   margin-top: 1em;
 }
+
+input, textarea {
+  background-color: whitesmoke;
+  margin: 0;
+  padding: 10px;
+  border: none;
+  width: 100%;
+  color: #2c3e50;
+  border-radius: 10px;
+}
+
+textarea {
+  resize: none;
+}
+
+label {
+  font-weight: 700;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.text-left {
+  text-align: left;
+}
+
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import AddQuestion from './views/Add-question.vue'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/add-question',
+      name: 'add question',
+      component: AddQuestion
     }
   ]
 })
