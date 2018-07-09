320256fce7f3914fb8ce205843386f01cefb02e0
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-07-08 19:12:49 -0500

    Defined the initial state of the layout :rocket:

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+	<vs-topbar vs-color="primary">
+		<div style="justify-content: flex-start">
+			<h2>
+				<router-link to='/'>Eastudy</router-link>
+			</h2>
+		</div>
+		<div style="justify-content: flex-end;">
+			<vs-button vs-color-text="rgb(255, 255, 255)" vs-color="rgba(255, 255, 255, 0.3)" vs-type="flat" >Preguntas</vs-button>
+			<vs-button vs-color-text="rgb(255, 255, 255)" vs-color="rgba(255, 255, 255, 0.3)" vs-type="flat" >Quiz</vs-button>
+		</div>
+	</vs-topbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	a {
+		color: white;
+	}
+	a:hover {
+		color: rgb(200, 200, 200);
+	}
+</style>
