c5c474d354bb510c1b5c42e38b972a27b5be5523
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-07-26 16:31:55 -0500

    Added the routes to the 404 view

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import AddQuestion from './views/Add-question.vue'
 import Question from './views/Question'
+import PageNotFound from './views/PageNotFound'
 
 Vue.use(Router)
 
@@ -23,6 +24,16 @@ export default new Router({
       path: '/question/:uid',
       name: 'question view',
       component: Question
+    },
+    {
+      path: ['/404', '*'],
+      name: '404 Page not found',
+      component: PageNotFound
+    },
+    {
+      path: '*',
+      name: '404 Page not found',
+      component: PageNotFound
     }
   ]
 })
