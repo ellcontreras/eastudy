9a1606f20e731d500231497920a4f2251affc264
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-08-07 23:51:49 -0500

    Created

---
+++ src/components/QuizCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <vs-card vs-color="secondary">
+        <vs-card-header :vs-title="name" vs-background-color="success" :vs-fill="true"/>
+        <vs-card-media :vs-media="img"></vs-card-media>        
+        <vs-card-body>
+            <span>
+                Demuestra cuanto sabes de {{ name }}
+            </span>
+        </vs-card-body>
+    </vs-card>
+</template>
+
+<script>
+export default {
+    name: 'QuizCard',
+    props: {
+        name: String,
+        img: String
+    }
+}
+</script>
+
+<style scoped>
+    img {
+        height: 10px !important;
+        min-height: 10px !important;
+        max-height: 10px !important;
+    }
+</style>
+
