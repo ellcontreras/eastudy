53bf3350a378168c9305de3f96bb3b2497d496ca
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-08-13 22:03:12 -0500

    Update by add library animate.css and new color themes

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css'
 
 import 'material-icons/iconfont/material-icons.css'
+import 'animate.css/animate.min.css'
 
 import firebase from 'firebase';
 
@@ -24,18 +25,18 @@ Vue.config.productionTip = false
 Vue.use(Vuesax, {
   theme:{
     colors:{
-      primary:'#5b3cc4',
+      primary:'#1d97c1',
       success:'rgb(23, 201, 100)',
       danger:'rgb(242, 19, 93)',
       warning:'rgb(255, 130, 0)',
-      dark:'rgb(36, 33, 69)',
-      green: '#45bf55',
-      yellow: '#edad1e',
-      blue: '#4472c4'
+      dark:'#393e46',
+      green: '#a3d444',
+      yellow: '#f8b500',
+      blue: '#118df0',
+      light: "#f7f7f7"
     }
   }
 })
-
 
 new Vue({
   router,
