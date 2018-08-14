90ccb485fefcd77ee9b1466547c257cf507ee453
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-08-13 22:05:54 -0500

    View to show the login component

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <vs-card class="text-center">
+        <vs-card-body>
+            <h1>Inicia Sesión</h1>
+            <br>
+            <Login></Login>
+        </vs-card-body>
+    </vs-card>
+</template>
+
+<script>
+import firebase from 'firebase'
+import Login from '@/components/Login.vue'
+
+export default {
+    name: 'LoginView',
+    components: {
+        Login
+    },
+    beforeCreate () {
+        if (firebase.auth().currentUser) {
+            this.$router.push('/')
+        }
+    }
+}
+</script>
+
