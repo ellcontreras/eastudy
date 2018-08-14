4d9e97b08307d7db999a3ef58ce4dff9e7b1ab80
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-08-13 22:05:13 -0500

    New component to show the user activity on profile

---
+++ src/components/Activity.vue
@@ -0,0 +1,43 @@
+<template>
+    <vs-card>
+        <vs-card-body>
+            <h2>{{ title }}</h2>
+            <small>{{ date.seconds | formatDate }}</small>
+            <p>{{ content }}</p>        
+        </vs-card-body>
+    </vs-card>
+</template>
+
+<script>
+export default {
+    name: 'Activity',
+    props: {
+        title: String,
+        content: String,
+        date: String
+    },
+    filters: {
+        formatDate(date) {
+            date = new Date(date * 1000)
+            var day = date.getDate()
+            var month = date.getMonth()
+            var year = date.getFullYear()
+
+            var hour = date.getHours()
+            var minutes = date.getMinutes()
+
+            if (hour > 12) { 
+                var format = "PM"
+            } else {
+                var format = "AM"
+            }
+
+            if (minutes < 10) {
+                minutes = '0' + minutes
+            }
+
+            return `${hour}:${minutes} ${format} - ${day}/${month}/${year}`
+        }
+    }
+}
+</script>
