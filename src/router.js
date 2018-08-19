3f7b3899a052722db8af772ff419ce3738c45628
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-08-18 23:59:40 -0500

    #5 Add route for the questions view

--- src/router.js
+++ src/router.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import AddQuestion from './views/Add-question.vue'
-import Question from './views/Question'
-import PageNotFound from './views/PageNotFound'
-import Login from './views/Login'
-import Profile from './views/Profile'
+import Question from './views/Question.vue'
+import Questions from './views/Questions.vue'
+import PageNotFound from './views/PageNotFound.vue'
+import Login from './views/Login.vue'
+import Profile from './views/Profile.vue'
 
 Vue.use(Router)
 
@@ -36,6 +37,11 @@ export default new Router({
       path: '/question/:uid',
       name: 'question view',
       component: Question
+    },
+    {
+      path: '/questions',
+      name: 'questions view',
+      component: Questions
     },
     {
       path: '/404',
