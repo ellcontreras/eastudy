84f08cca084a93765b4614bfa3f0e41bbc0c1b25
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-08-30 11:40:03 -0500

    Remove the vuesax info

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
-import Vuesax from 'vuesax'
-import 'vuesax/dist/vuesax.css'
-
+import 'bulma/css/bulma.min.css'
 import 'material-icons/iconfont/material-icons.css'
 import 'animate.css/animate.min.css'
 
@@ -21,22 +19,6 @@ firebase.initializeApp({
 })
 
 Vue.config.productionTip = false
-
-Vue.use(Vuesax, {
-  theme:{
-    colors:{
-      primary:'#1d97c1',
-      success:'rgb(23, 201, 100)',
-      danger:'rgb(242, 19, 93)',
-      warning:'rgb(255, 130, 0)',
-      dark:'#393e46',
-      green: '#a3d444',
-      yellow: '#f8b500',
-      blue: '#118df0',
-      light: "#f7f7f7"
-    }
-  }
-})
 
 new Vue({
   router,
