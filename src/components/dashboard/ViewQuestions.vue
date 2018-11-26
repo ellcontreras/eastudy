74ca898d00479feb6d15a33aab0ea1674b981ad3
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-11-26 13:42:46 -0600

    #6 created component to view all the questions

---
+++ src/components/dashboard/ViewQuestions.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="columns">
+            <div class="column is-3 ">
+                <menu-dashboard></menu-dashboard>
+            </div>
+            <div class="column is-9">
+                <div class="hero is-primary">
+                    <div class="hero-body">
+                        <h2 class="title is-2 has-text-centered">Lista de preguntas</h2>
+                    </div>
+                </div>
+
+                <hr>
+
+                <ul class="menu-list">
+                    <li v-for="(q, k) of questions" :key="k"><a>{{ q }}</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import MenuDashboard from './MenuDashboard.vue'
+
+export default {
+    name: 'ViewQuestions',
+    components: { MenuDashboard},
+    data() {
+        return {
+            questions: ['123123', '12341234', '12341234', '12341234']
+        }
+    }
+}
+</script>
