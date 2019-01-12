ca0e435ae327450db0006897142d4c64ece47d82
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-01-12 00:23:06 -0600

    Created home manager to display different view

---
+++ src/views/HomeManager.vue
@@ -0,0 +1,14 @@
+<template>
+  <home v-if="$firebase.auth().currentUser"/>
+  <home-presentation v-else/>
+</template>
+
+<script>
+import HomePresentation from "./HomePresentation.vue";
+import Home from "./Home.vue";
+
+export default {
+  name: "HomeManager",
+  components: { HomePresentation, Home }
+};
+</script>
