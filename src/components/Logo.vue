b62bd55b0c62145e2bcfa9a1712cec043641d0da
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-01-28 11:56:41 -0600

    Created component that display a logo :yum:

---
+++ src/components/Logo.vue
@@ -0,0 +1,34 @@
+<template>
+  <img
+    src="@/assets/eastudy.svg"
+    :class="{'normal' : where === 'normal', 'navbar' : where === 'navbar'}"
+    alt="eastudy logo"
+  >
+</template>
+
+<script>
+export default {
+  name: "Logo",
+  props: {
+    where: { type: String, default: "normal" }
+  }
+};
+</script>
+
+
+<style scoped>
+img {
+  background: transparent;
+}
+
+.normal {
+  width: 1380px;
+  height: 380px;
+}
+
+.navbar {
+  width: 230px;
+  height: 63px;
+}
+</style>
+
